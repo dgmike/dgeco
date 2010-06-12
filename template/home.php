499f2ff6aca8197f24522e32a2dd44728fe9c48a
Michael <michaelgranados@gmail.com>
2010-06-12 07:47:58 -0300

    Adicionando botao de comprar e link para a imagem

--- template/home.php
+++ template/home.php
@@ -1,14 +1,13 @@
 
 <h1>Produtos em destaque</h1>
 
-
-
 <?php foreach($produtos as $p): settype($p, 'object') ?>
 
 <dl>
-    <img src="<?php echo url('image/'.$p->slug); ?>" width="150" height="150" />
+    <a href="<?php echo url('produto/'.$p->slug); ?>" title="Produto: <?php echo $p->nome; ?>"><img src="<?php echo url('image/'.$p->slug); ?>" width="150" height="150" /></a>
     <dt><a href="<?php echo url('produto/'.$p->slug); ?>" title="Produto: <?php echo $p->nome; ?>"><?php echo $p->nome; ?></a></dt>
     <dd>Preço: <strong><?php echo $p->preco; ?></strong></dd>
+    <dd><a href="<?php echo url('adicionar/'.$p->slug); ?>" title="Adicionar este produto ao carrinho">Comprar</a></dd>
 </dl>
 
 <?php endforeach ?>
