e51565665ab921101402f89472174eb8a2247cc4
Michael <michaelgranados@gmail.com>
2010-06-12 07:13:59 -0300

    Criando template para a home

--- template/home.php
+++ template/home.php
@@ -1,3 +1,15 @@
 
 <h1>Produtos em destaque</h1>
 
+
+
+<?php foreach($produtos as $p): settype($p, 'object') ?>
+
+<dl>
+    <img src="<?php echo url('image/'.$p->slug); ?>" width="150" height="150" />
+    <dt><a href="<?php echo url('produto/'.$p->slug); ?>" title="Produto: <?php echo $p->nome; ?>"><?php echo $p->nome; ?></a></dt>
+    <dd>Preço: <strong><?php echo $p->preco; ?></strong></dd>
+</dl>
+
+<?php endforeach ?>
+
